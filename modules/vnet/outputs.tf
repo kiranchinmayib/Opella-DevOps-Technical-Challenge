da01d8b3f770109071c6b2890e112166506df9c8
kiranchinmayib <kiranchinmayib@gmail.com>
2026-04-02 13:08:53 +0530

    Update outputs.tf

--- modules/vnet/outputs.tf
+++ modules/vnet/outputs.tf
@@ -1,16 +1,29 @@
-output "vnet_id" {
-  description = "VNET ID"
-  value       = azurerm_virtual_network.this.id
+output "resource_group_name" {
+  description = "Name of the resource group"
+  value       = azurerm_resource_group.rg.name
 }
 
-output "vnet_name" {
-  value = azurerm_virtual_network.this.name
+output "vnet_id" {
+  description = "Virtual Network ID"
+  value       = module.vnet.vnet_id
 }
 
 output "subnet_ids" {
-  description = "Subnet IDs map"
-  value = {
-    for subnet_name, subnet in azurerm_subnet.this :
-    subnet_name => subnet.id
-  }
+  description = "Subnet IDs from VNET module"
+  value       = module.vnet.subnet_ids
+}
+
+output "storage_account_name" {
+  description = "Storage Account name"
+  value       = azurerm_storage_account.sa.name
+}
+
+output "vm_name" {
+  description = "Virtual Machine name"
+  value       = azurerm_linux_virtual_machine.vm.name
+}
+
+output "vm_private_ip" {
+  description = "Private IP of VM"
+  value       = azurerm_network_interface.nic.private_ip_address
 }
